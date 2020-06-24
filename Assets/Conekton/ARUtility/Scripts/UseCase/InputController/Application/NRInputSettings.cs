a0e7e929756aa7f94e718e7782a3f8b48bc00b34
edom18 <edo.m18@gmai.com>
2020-06-24 15:30:12 +0900

    feat: Added two base classes for input settings.

---
+++ Assets/Conekton/ARUtility/Scripts/UseCase/InputController/Application/NRInputSettings.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_ANDROID && PLATFORM_NREAL
+using NRKernal;
+#endif
+
+namespace Conekton.ARUtility.Input.Application
+{
+    [CreateAssetMenu(fileName = "NRInputSettings", menuName = "ARUtility/NRInputSettings")]
+    public class NRInputSettings : ScriptableObject
+    {
+#if UNITY_ANDROID && PLATFORM_NREAL
+        [SerializeField] public ControllerButton One = ControllerButton.APP;
+        [SerializeField] public ControllerButton Two = ControllerButton.GRIP;
+        [SerializeField] public ControllerButton Three = ControllerButton.HOME;
+        [SerializeField] public ControllerButton Four = ControllerButton.TOUCHPAD_BUTTON;
+#endif
+    }
+}
+
