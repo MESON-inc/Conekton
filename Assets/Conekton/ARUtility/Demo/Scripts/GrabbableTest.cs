bf130e2b4547d29c89a2ec5de813469fef34de4c
edom18 <edo.m18@gmail.com>
2020-08-17 19:12:57 +0900

    feat: Added a hand grab demo.

---
+++ Assets/Conekton/ARUtility/Demo/Scripts/GrabbableTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Conekton.ARUtility.HandGrabSystemUseCase.Domain;
+using UnityEngine;
+
+namespace Conekton.ARUtility.Demo
+{
+    public class GrabbableTest : MonoBehaviour
+    {
+        private IGrabbable _grabbable = null;
+
+        private void Awake()
+        {
+            var ren = GetComponent<Renderer>();
+            var material = ren.material;
+            
+            _grabbable = GetComponent<IGrabbable>();
+            _grabbable.OnTouched += (grabber, grabbable) =>
+            {
+                material.color = Color.red;
+            };
+
+            _grabbable.OnUntouched += (grabber, grabbable) =>
+            {
+                material.color = Color.white;
+            };
+
+            _grabbable.OnBeganGrab += (grabbere, grabbable) =>
+            {
+                material.color = Color.blue;
+            };
+
+            _grabbable.OnEndedGrab += (grabbere, grabbable) =>
+            {
+                material.color = Color.green;
+            };
+        }
+    }
+}
