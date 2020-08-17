c6647ddc2086e3a7bad946ae1ee05cca5009f364
edom18 <edo.m18@gmail.com>
2020-08-17 17:17:46 +0900

    feat: Implemented some stuffs of the hand system for the oculus quest.

---
+++ Assets/Conekton/ARUtility/Scripts/UseCase/HandGrabSystem/Infrastructure/Oculus/OculusHandGrabController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+using Conekton.ARUtility.HandSystemUseCase.Domain;
+using Conekton.ARUtility.HandGrabSystemUseCase.Domain;
+
+namespace Conekton.ARUtility.HandGrabSystemUseCase.Infrastructure
+{
+    public class OculusHandGrabController : MonoBehaviour, IHandGrabController
+    {
+        private IHandGrabSystem _grabSystem = null;
+        private IHandSystem _handSystem = null;
+
+        private IGrabber _grabber = null;
+        private IHand _hand = null;
+
+        private bool _injected = false;
+
+        [Inject]
+        private void Injection(IHandGrabSystem grabSystem, IHandSystem handSystem)
+        {
+            _grabSystem = grabSystem;
+            _handSystem = handSystem;
+
+            _grabber = GetComponent<IGrabber>();
+            _hand = GetComponent<IHand>();
+            
+            _grabber.OnTouched += HandleGrabberOnOnTouched;
+            _grabber.OnUntouched += HandleGrabberOnOnUntouched;
+
+            _injected = true;
+        }
+
+        private void HandleGrabberOnOnTouched(IGrabber grabber, IGrabbable grabbable)
+        {
+            _grabSystem.Touched(grabber, grabbable);
+        }
+
+        private void HandleGrabberOnOnUntouched(IGrabber grabber, IGrabbable grabbable)
+        {
+            _grabSystem.Untouched(grabber, grabbable);
+        }
+
+        #region ### MonoBehaviour ###
+        private void Update()
+        {
+            if (!_injected)
+            {
+                return;
+            }
+
+            if (_hand.GetFingerIsPinching(FingerType.Index))
+            {
+                Grab();
+            }
+        }
+        #endregion ### MonoBehaviour ###
+
+        private void Grab()
+        {
+            foreach (var grabbable in _grabber.GetTargetGrabbables())
+            {
+                _grabSystem.BeginGrab(_grabber, grabbable);
+            }
+        }
+    }
+}
---
+++ Assets/Conekton/ARUtility/Scripts/UseCase/HandGrabSystem/Infrastructure/Oculus/OculusHandGrabber.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Conekton.ARUtility.HandGrabSystemUseCase.Domain;
+
+namespace Conekton.ARUtility.HandGrabSystemUseCase.Infrastructure
+{
+    public class OculusHandGrabber : MonoBehaviour, IGrabber
+    {
+        public event OnTouchedEvent OnTouched;
+        public event OnUntouchedEvent OnUntouched;
+        
+        private readonly List<IGrabbable> _targetGrabbables = new List<IGrabbable>();
+
+        #region ### MonoBehaviour ###
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.TryGetComponent(out IGrabbable grabbable))
+            {
+                AddGrabbable(grabbable);
+                OnTouched?.Invoke(this, grabbable);
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.TryGetComponent(out IGrabbable grabbable))
+            {
+                RemoveGrabbable(grabbable);
+                OnUntouched?.Invoke(this, grabbable);
+            }
+        }
+        #endregion ### MonoBehaviour ###
+
+        #region ### IGrabber interface ###
+
+        IReadOnlyList<IGrabbable> IGrabber.GetTargetGrabbables()
+        {
+            return _targetGrabbables;
+        }
+        
+        private void AddGrabbable(IGrabbable grabbable)
+        {
+            if (!_targetGrabbables.Contains(grabbable))
+            {
+                _targetGrabbables.Add(grabbable);
+            }
+        }
+
+        private void RemoveGrabbable(IGrabbable grabbable)
+        {
+            if (_targetGrabbables.Contains(grabbable))
+            {
+                _targetGrabbables.Remove(grabbable);
+            }
+        }
+
+        void IGrabber.Grab(IGrabbable grabbable)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        void IGrabber.Ungrab(IGrabbable grabbable)
+        {
+            throw new System.NotImplementedException();
+        }
+        #endregion ### IGrabber interface ###
+    }
+}
