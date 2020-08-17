2b84745b6d7aecc0d6ddc117ea881e059e2c45ae
edom18 <edo.m18@gmail.com>
2020-08-17 11:25:54 +0900

    feat: Added a definition for a hand-grab-system.

---
+++ Assets/Conekton/ARUtility/Scripts/UseCase/HandGrabSystem/Domain/HandGrabSystemDomain.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Conekton.ARUtility.HandGrabSystemUseCase.Domain
+{
+    public delegate void OnBeganGrabEvent(IGrabber grabbere, IGrabbable grabbable);
+    public delegate void OnMovedGrabEvent(IGrabber grabbere, IGrabbable grabbable);
+    public delegate void OnEndedGrabEvent(IGrabber grabbere, IGrabbable grabbable);
+    public delegate void OnForceEndedGrabEvent(IGrabbable grabbable);
+    public delegate void OnPausedGrabEvent();
+    public delegate void OnResumedGrabEvent();
+    
+    public interface IHandGrabSystem
+    {
+        void Touched(IGrabber grabber, IGrabbable grabbable);
+        void Untouched(IGrabber grabber, IGrabbable grabbable);
+        void BeginGrab(IGrabber grabber, IGrabbable grabbable);
+        void MoveGrab(IGrabber grabber, IGrabbable grabbable);
+        void EndGrab(IGrabber grabber, IGrabbable grabbable);
+    }
+
+    public interface IGrabber
+    {
+        void Grab(IGrabbable grabbable);
+        void Ungrab(IGrabbable grabbable);
+    }
+
+    public interface IGrabbable
+    {
+        bool IsGrabbed { get; }
+        void Begin(IGrabber grabber);
+        void Move(IGrabber grabber);
+        void End(IGrabber grabber);
+        void ForceEnd();
+        void Pause();
+        void Resume();
+    }
+    
+    public interface IGrabController {}
+}
