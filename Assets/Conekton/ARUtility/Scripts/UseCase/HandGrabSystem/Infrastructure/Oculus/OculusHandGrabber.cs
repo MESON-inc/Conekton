76a5445c80c9436b0c3f4120d172753ac1ce6bfc
edom18 <edo.m18@gmail.com>
2020-08-18 11:48:14 +0900

    feat: Care to remove the target list.

--- Assets/Conekton/ARUtility/Scripts/UseCase/HandGrabSystem/Infrastructure/Oculus/OculusHandGrabber.cs
+++ Assets/Conekton/ARUtility/Scripts/UseCase/HandGrabSystem/Infrastructure/Oculus/OculusHandGrabber.cs
@@ -2,8 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 using Conekton.ARUtility.HandGrabSystemUseCase.Domain;
+using ModestTree;
 
 namespace Conekton.ARUtility.HandGrabSystemUseCase.Infrastructure
 {
@@ -11,16 +11,18 @@ namespace Conekton.ARUtility.HandGrabSystemUseCase.Infrastructure
     {
         public event OnTouchedEvent OnTouched;
         public event OnUntouchedEvent OnUntouched;
-        
+
         private readonly List<IGrabbable> _targetGrabbables = new List<IGrabbable>();
+        private readonly List<IGrabbable> _grabbedGrabbables = new List<IGrabbable>();
+        private readonly List<IGrabbable> _removeTarget = new List<IGrabbable>();
 
         #region ### MonoBehaviour ###
+
         private void OnTriggerEnter(Collider other)
         {
             if (other.TryGetComponent(out IGrabbable grabbable))
             {
-                AddGrabbable(grabbable);
-                OnTouched?.Invoke(this, grabbable);
+                TryTouch(grabbable);
             }
         }
 
@@ -28,13 +30,20 @@ namespace Conekton.ARUtility.HandGrabSystemUseCase.Infrastructure
         {
             if (other.TryGetComponent(out IGrabbable grabbable))
             {
-                RemoveGrabbable(grabbable);
-                OnUntouched?.Invoke(this, grabbable);
+                TryUntouch(grabbable);
             }
         }
+
+        private void LateUpdate()
+        {
+            _targetGrabbables.RemoveAll(g => _removeTarget.Contains(g));
+            _removeTarget.Clear();
+        }
+
         #endregion ### MonoBehaviour ###
 
         #region ### IGrabber interface ###
+
         public bool IsGrabbed { get; private set; } = false;
 
         public IReadOnlyList<IGrabbable> GetTargetGrabbables()
@@ -45,11 +54,49 @@ namespace Conekton.ARUtility.HandGrabSystemUseCase.Infrastructure
         public void Grab(IGrabbable grabbable)
         {
             IsGrabbed = true;
+
+            if (!_grabbedGrabbables.Contains(grabbable))
+            {
+                _grabbedGrabbables.Add(grabbable);
+            }
         }
 
         public void Ungrab(IGrabbable grabbable)
         {
             IsGrabbed = false;
+
+            if (_grabbedGrabbables.Contains(grabbable))
+            {
+                _grabbedGrabbables.Remove(grabbable);
+            }
+
+            TryUntouch(grabbable);
+        }
+
+        private bool TryTouch(IGrabbable grabbable)
+        {
+            if (!CanTouch(grabbable))
+            {
+                return false;
+            }
+
+            AddGrabbable(grabbable);
+            OnTouched?.Invoke(this, grabbable);
+
+            return true;
+        }
+
+        private bool TryUntouch(IGrabbable grabbable)
+        {
+            if (!CanUntouch(grabbable))
+            {
+                return false;
+            }
+
+            RemoveGrabbable(grabbable);
+            OnUntouched?.Invoke(this, grabbable);
+
+            return true;
         }
 
         public Pose GetPose()
@@ -57,8 +104,9 @@ namespace Conekton.ARUtility.HandGrabSystemUseCase.Infrastructure
             Transform trans = transform;
             return new Pose(trans.position, trans.rotation);
         }
+
         #endregion ### IGrabber interface ###
-        
+
         private void AddGrabbable(IGrabbable grabbable)
         {
             if (!_targetGrabbables.Contains(grabbable))
@@ -71,8 +119,21 @@ namespace Conekton.ARUtility.HandGrabSystemUseCase.Infrastructure
         {
             if (_targetGrabbables.Contains(grabbable))
             {
-                _targetGrabbables.Remove(grabbable);
+                if (!_removeTarget.Contains(grabbable))
+                {
+                    _removeTarget.Add(grabbable);
+                }
             }
+        }
+
+        private bool CanTouch(IGrabbable grabbable)
+        {
+            return !_targetGrabbables.Contains(grabbable);
+        }
+
+        private bool CanUntouch(IGrabbable grabbable)
+        {
+            return !_grabbedGrabbables.Contains(grabbable);
         }
     }
 }
