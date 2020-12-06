28c6a8e354f5b21d356412f824e7da5b93515d25
edom18 <edo.m18@gmail.com>
2020-12-06 11:53:24 +0900

    feat: Added a new PCA called FixedPCA.
    It will be used a PCA as fixed.

---
+++ Assets/Conekton/Multiplayer/Scripts/UseCase/PersistentCoordinate/Presentation/FixedPCA.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Conekton.ARMultiplayer.PersistentCoordinate.Domain;
+using Zenject;
+
+namespace Conekton.ARMultiplayer.PersistentCoordinate.Infrastructure
+{
+    public class FixedPCA : MonoBehaviour, IPCA
+    {
+        [Inject] private IPersistentCoordinateService _persistentCoordinateService = null;
+        
+        [SerializeField] private Priority _priority = Priority.High;
+        [SerializeField] private string _uniqueID = "";
+        
+        private void Awake()
+        {
+            if (string.IsNullOrEmpty(_uniqueID))
+            {
+                Debug.LogWarning("Unique ID is not set. This may be problem for calculating persistent coordinate.", gameObject);
+            }
+            else
+            {
+                _persistentCoordinateService.Register(this);
+            }
+        }
+
+        PCAID IPCA.ID { get; set; }
+
+        string IPCA.UniqueID => _uniqueID;
+
+        Priority IPCA.Priority => _priority;
+
+        Vector3 IPCA.Position => transform.position;
+
+        Quaternion IPCA.Rotation => transform.rotation;
+    }
+}
+
