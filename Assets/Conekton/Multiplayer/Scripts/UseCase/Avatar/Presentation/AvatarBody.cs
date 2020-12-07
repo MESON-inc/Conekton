a5ea42d18e520790bd6404afc4a6488113a5e6c4
edom18 <edo.m18@gmai.com>
2020-12-07 20:50:46 +0900

    feat: Updated each transform in update timing.

--- Assets/Conekton/Multiplayer/Scripts/UseCase/Avatar/Presentation/AvatarBody.cs
+++ Assets/Conekton/Multiplayer/Scripts/UseCase/Avatar/Presentation/AvatarBody.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,6 +12,10 @@ namespace Conekton.ARMultiplayer.AvatarBody.Presentation
 {
     public class AvatarBody : MonoBehaviour, IAvatarBody
     {
+        [SerializeField] private Transform _headTrans = null;
+        [SerializeField] private Transform _leftHandTrans = null;
+        [SerializeField] private Transform _rightHandTrans = null;
+        
         public AvatarBodyType BodyType => _bodyType;
         public AvatarBodyID BodyID => _bodyID;
         public Transform Transform => transform;
@@ -18,6 +23,29 @@ namespace Conekton.ARMultiplayer.AvatarBody.Presentation
         private AvatarBodyType _bodyType = AvatarBodyType.A;
         private AvatarBodyID _bodyID = default;
         private IAvatar _avatar = null;
+        private bool _hasAvatar = false;
+
+        private void Update()
+        {
+            UpdatePose();
+        }
+
+        private void UpdatePose()
+        {
+            if (!_hasAvatar)
+            {
+                return;
+            }
+
+            Pose headPose = _avatar.GetPose(AvatarPoseType.Head);
+            _headTrans.SetPositionAndRotation(headPose.position, headPose.rotation);
+            
+            Pose leftPose = _avatar.GetPose(AvatarPoseType.Left);
+            _leftHandTrans.SetPositionAndRotation(leftPose.position, leftPose.rotation);
+            
+            Pose rightPose = _avatar.GetPose(AvatarPoseType.Right);
+            _rightHandTrans.SetPositionAndRotation(rightPose.position, rightPose.rotation);
+        }
 
         [Inject]
         public void Construct(AvatarBodyTypeArgs args)
@@ -34,6 +62,7 @@ namespace Conekton.ARMultiplayer.AvatarBody.Presentation
         public void SetAvatar(IAvatar avatar)
         {
             _avatar = avatar;
+            _hasAvatar = avatar != null;
         }
     }
 }
