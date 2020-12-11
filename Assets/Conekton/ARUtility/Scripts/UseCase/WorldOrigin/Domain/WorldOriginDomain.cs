7a6b8f76999dc1ad878ea9b7180296c4db51d0db
edom18 <edo.m18@gmai.com>
2020-12-11 11:47:54 +0900

    doc: Added comments.

--- Assets/Conekton/ARUtility/Scripts/UseCase/WorldOrigin/Domain/WorldOriginDomain.cs
+++ Assets/Conekton/ARUtility/Scripts/UseCase/WorldOrigin/Domain/WorldOriginDomain.cs
@@ -6,6 +6,11 @@ namespace Conekton.ARUtility.UseCase.WorldOrigin.Domain
 {
     public delegate void MovedEvent(IWorldOrigin origin);
     
+    /// <summary>
+    /// IWorldOrigin interface represents a content origin pose.
+    ///
+    /// All of networked objects are based on this origin.
+    /// </summary>
     public interface IWorldOrigin
     {
         event MovedEvent OnMoved;
@@ -14,6 +19,10 @@ namespace Conekton.ARUtility.UseCase.WorldOrigin.Domain
         void Clear();
     }
 
+    /// <summary>
+    /// IworldAnchor interface represents an anchor based on something.
+    /// It depends on purpose to use.
+    /// </summary>
     public interface IWorldAnchor
     {
         Pose RelativePose { get; }
