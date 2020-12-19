b8e8c1609d1972d14afe4e309e1854f3cd7f24cd
edom18 <edo.m18@gmail.com>
2020-12-19 13:57:43 +0900

    feat: ARMarker ID solver bindings moved to the marker detector installer and deleted its prefab.

--- Assets/Conekton/ARUtility/Scripts/Installers/ARMarkerDetectorInstaller.cs
+++ Assets/Conekton/ARUtility/Scripts/Installers/ARMarkerDetectorInstaller.cs
@@ -1,8 +1,13 @@
 using UnityEngine;
 using Zenject;
-
 using Conekton.ARUtility.UseCase.ARMarkerDetector.Domain;
 using Conekton.ARUtility.UseCase.ARMarkerDetector.Infrastructure;
+using Conekton.ARUtility.UseCase.ARMarkerIDSolver.Domain;
+using Conekton.ARUtility.UseCase.ARMarkerIDSolver.Infrastructure;
+
+#if (UNITY_IOS || UNITY_ANDROID) && !(PLATFORM_NREAL || PLATFORM_OCULUS)
+using UnityEngine.XR.ARSubsystems;
+#endif
 
 namespace Conekton.ARUtility.UseCase.ARMarkerDetector.Application
 {
@@ -11,30 +16,96 @@ namespace Conekton.ARUtility.UseCase.ARMarkerDetector.Application
         [SerializeField] private GameObject _editorMarkerDetectorPrefab = null;
         [SerializeField] private GameObject _MLARMarkerDetectorPrefab = null;
 
+        [SerializeField] private bool _useEditorDetector = true;
+
         public override void InstallBindings()
         {
+            Container
+                .Bind<IMarkerIDRepository>()
+                .To<MarkerIDRepository>()
+                .AsCached();
+
 #if UNITY_EDITOR
+            if (_useEditorDetector)
+            {
+                BindEditorDependency();
+                return;
+            }
+#endif
+
+#if UNITY_ANDROID
+    #if PLATFORM_NREAL
+            BindNrealDependency();
+    #elif PLATFORM_OCULUS
             BindEditorDependency();
-#elif UNITY_ANDROID && PLATFORM_NREAL
-            Container.BindInterfacesAndSelfTo<NRARMarkerDetector>().AsCached();
-#elif UNITY_ANDROID && PLATFORM_OCULUS
-            BindEditorDependency();
-#elif UNITY_IOS || UNITY_ANDROID
-            Container.Bind<ARTrackedImageManagerProvider>().AsSingle();
-            Container.BindInterfacesAndSelfTo<MobileARMarkerDetector>().AsCached();
+    #else
+            BindMobileDependency();
+    #endif
+#elif UNITY_IOS
+            BindMobileDependency();
 #elif PLATFORM_LUMIN
+            BindMagicLeapDependency();
+#else
+            BindEditorDependency();
+#endif
+        }
+
+        #region ### For detector ###
+
+#if UNITY_IOS || UNITY_ANDROID
+        private void BindMobileDependency()
+        {
+            Container
+                .Bind<IMarkerIDSolver<XRReferenceImage>>()
+                .To<MobileMarkerIDSolver>()
+                .AsCached();
+            
+            Container
+                .Bind<ARTrackedImageManagerProvider>()
+                .AsSingle();
+            Container
+                .BindInterfacesAndSelfTo<MobileARMarkerDetector>()
+                .AsCached();
+        }
+#endif
+
+#if PLATFORM_NREAL
+        private void BindNrealDependency()
+        {
+            Container
+                .Bind<IMarkerIDSolver<int>>()
+                .To<NRMarkerIDSolver>()
+                .AsCached();
+            
+            Container
+                .BindInterfacesAndSelfTo<NRARMarkerDetector>()
+                .AsCached();
+        }
+#endif
+
+#if PLATFORM_LUMIN
+        private void BindMagicLeapDependency()
+        {
+            Container
+                .Bind<IMarkerIDSolver<string>>()
+                .To<MLMarkerIDSolver>()
+                .AsCached();
+
             Container
                 .BindInterfacesAndSelfTo<MLARMarkerDetector>()
                 .FromComponentInNewPrefab(_MLARMarkerDetectorPrefab)
                 .AsCached()
                 .NonLazy();
-#else
-            Container.Bind<IARMarkerDetector>().To<EditorARMarkerDetector>().AsCached();
-#endif
         }
+#endif
 
         private void BindEditorDependency()
         {
+            Container
+                .Bind<IMarkerIDSolver<int>>()
+                .To<EditorMarkerIDSolver>()
+                .AsCached();
+
             Container
                 .Bind<IARMarkerDetector>()
                 .To<EditorARMarkerDetector>()
@@ -42,5 +113,7 @@ namespace Conekton.ARUtility.UseCase.ARMarkerDetector.Application
                 .AsCached()
                 .NonLazy();
         }
+
+        #endregion ### For detector ###
     }
 }
