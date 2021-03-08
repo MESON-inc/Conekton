4004438537327d4101946772942618c614ce8245
edom18 <edo.m18@gmai.com>
2021-03-08 16:07:44 +0900

    feat: Added a new script for creating a symbolic link with selected platform.

---
+++ Assets/ConektonUtility/Editor/SymbolicLink.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using UnityEditor;
+using UnityEngine;
+using UEditorUtility = UnityEditor.EditorUtility;
+
+namespace Conekton.EditorUtility
+{
+    public static class SymbolicLinkCreator
+    {
+        public enum PlatformType
+        {
+            Nreal,
+            Oculus,
+        }
+        
+        public static void Create(PlatformType type)
+        {
+            (string folderName, string linkFolderName) = GetFolderNames(type);
+            
+            if (Directory.Exists(folderName))
+            {
+                return;
+            }
+
+            string arguments = $"/c mklink /D \"{folderName}\" \"{linkFolderName}\"";
+
+            Process proc = new Process();
+            proc.StartInfo.FileName = System.Environment.GetEnvironmentVariable("ComSpec");
+            proc.StartInfo.Arguments = arguments;
+            proc.StartInfo.Verb = "RunAs";
+            proc.Start();
+            proc.WaitForExit();
+            proc.Close();
+        }
+
+        private static (string, string) GetFolderNames(PlatformType type)
+        {
+            string SDKName = "";
+            
+            switch (type)
+            {
+                case PlatformType.Nreal:
+                    SDKName = "NRSDK";
+                    break;
+                
+                case PlatformType.Oculus:
+                    SDKName = "Oculus";
+                    break;
+            }
+            
+            string folderName = $"{Application.dataPath}/{SDKName}";
+            string linkFolderName = $"{Application.dataPath}/../{SDKName}";
+
+            return (folderName, linkFolderName);
+        }
+    }
+}
